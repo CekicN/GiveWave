e7cebd567179896922b92422ee1c3eb67c6d970a
CekicN <cekicnikola53@gmail.com>
2023-04-11 23:38:25 +0200

    Projekat

---
+++ Projekat/GiveWaveAPI/GiveWaveAPI/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GiveWaveAPI.Models
+{
+    public partial class User
+    {
+        public long ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+    }
+}
