16dd513c86983e97bdab24cbbdfe182cf465e10b
CekicN <cekicnikola53@gmail.com>
2023-04-13 21:06:38 +0200

    home

--- Aplikacija/GiveWave/src/app/app-routing.module.ts
+++ Aplikacija/GiveWave/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/user-auth/login/login.component';
+import { SignupComponent } from './components/user-auth/signup/signup.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
--- Aplikacija/GiveWave/src/app/app.module.ts
+++ Aplikacija/GiveWave/src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
-
 import { AppRoutingModule } from './app-routing.module';
+import {UserAuthModule} from './components/user-auth/user-auth.module';
+
 import { AppComponent } from './components/app/app.component';
 import { NavigationBarComponent } from './components/core/navigation-bar/navigation-bar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { FooterBarComponent } from './components/core/footer-bar/footer-bar.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -22,17 +16,11 @@ import { SignupComponent } from './components/signup/signup.component';
     AboutUsComponent,
     NavigationBarComponent,
     FooterBarComponent,
-    LoginComponent,
-    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule
+    UserAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
