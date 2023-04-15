9484583946be1f0255e6063c39adba7d6265fc88
CekicN <cekicnikola53@gmail.com>
2023-04-15 02:02:12 +0200

    PocetnaStrana

--- Aplikacija/GiveWave/src/app/components/core/footer-bar/footer-bar.component.ts
+++ Aplikacija/GiveWave/src/app/components/core/footer-bar/footer-bar.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer-bar.component.css']
 })
 export class FooterBarComponent {
-
+ 
 }
--- Aplikacija/GiveWave/src/app/components/home/home.component.ts
+++ Aplikacija/GiveWave/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import {faArrowRight, faArrowLeft} from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(library: FaIconLibrary)
+  {
+    library.addIcons(faArrowLeft, faArrowRight);
+  }
+  text:string[] = [
+    "a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut at eos minima repudiandae. Assumenda repudiandae neque distinctio, minima perferendis voluptas?",
+    "a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut at eos minima repudiandae. ",
+    "a Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut",
+    "orem ipsum, dolor sit amet consectetur adipisicing elit. Ut at eos minima repudiandae. Assumenda repudiandae neque distinctio, minima perferendis voluptas?"
+  ];
+  isActive = 1;
+  next() {
+    if (this.isActive == this.text.length) this.isActive = 0;
+    this.isActive ++;
+  }
+  pre() {
+    this.isActive --;
+    if (this.isActive == 0) this.isActive = this.text.length;
+  }
+  Active(t:string):boolean{
+    return (this.text.indexOf(t)+1) == this.isActive;
+  }
 }
