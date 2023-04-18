7027a307830c74c54c0b530f17bc93acedc31787
Stevan Ruzic <stevan.ruzic@elfak.rs>
2023-04-18 15:58:11 +0200

    pusss

--- Aplikacija/GiveWaveAPI/GiveWaveAPI/Controllers/KategorijaController.cs
+++ Aplikacija/GiveWaveAPI/GiveWaveAPI/Controllers/KategorijaController.cs
@@ -1,6 +1,42 @@
-﻿namespace GiveWaveAPI.Controllers
+﻿using GiveWaveAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace GiveWaveAPI.Controllers
 {
-    public class KategorijaController
+    [ApiController]
+    [Route("controller")]
+    public class KategorijaController:ControllerBase
     {
+        public GiveWaveDBContext Context { get; set; }
+        public KategorijaController(GiveWaveDBContext context) 
+        {
+            Context = context;
+        }
+
+        [Route("DodajKategoriju")]
+        [HttpPost]
+        public async Task<ActionResult> dodajKategoriju([FromBody] Kategorija kategorija)
+        {
+            try
+            {
+                Context.Kategorijas.Add(kategorija);
+                await Context.SaveChangesAsync();
+                return Ok("Kategorija je dodata");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("PreuzmiKategoriju")]
+        [HttpGet]
+
+        public async Task<ActionResult> preuzmi()
+        {
+            return Ok(Context.Kategorijas);
+        }
+
     }
 }
