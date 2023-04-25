4137d96f4d1af676a5529c109f83d407f718d739
CekicN <cekicnikola53@gmail.com>
2023-04-25 04:02:03 +0200

    edit

--- Aplikacija/GiveWave/src/app/components/profile/profile-data/profile-data.component.ts
+++ Aplikacija/GiveWave/src/app/components/profile/profile-data/profile-data.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile.service';
 import { User } from 'app/Models/User';
-import {faInstagram, faFacebook, faTwitter, faGithub} from '@fortawesome/free-brands-svg-icons';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { IconName, faFloppyDisk, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile-data',
@@ -12,8 +12,9 @@ import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 export class ProfileDataComponent implements OnInit {
   
   public user!:User;
+  icon:IconName = 'pen-to-square';
   constructor(private service:ProfileService, library:FaIconLibrary){
-    library.addIcons(faInstagram, faFacebook, faTwitter, faGithub);
+    library.addIcons(faPenToSquare, faFloppyDisk);
   }
   ngOnInit(): void {
     this.service.getUsersById(1).subscribe(user => {
@@ -21,5 +22,21 @@ export class ProfileDataComponent implements OnInit {
       this.service.email = user.email
     })
   }
-  
+  EditData(event:Event)
+  {
+    const edit = this.icon === 'pen-to-square'?"true" :"false";
+    const kliknut = event.target as HTMLElement;
+    const input = kliknut.closest('div')?.querySelector('p');
+    input!.setAttribute('contenteditable', edit);
+
+    this.icon = this.icon === 'floppy-disk' ? 'pen-to-square' : 'floppy-disk';
+    if(this.icon === 'floppy-disk')
+    {
+      this.SaveChanges(input);
+    }
+  }
+  SaveChanges(input:HTMLElement|undefined|null)
+  {
+    //metoda koja cuva izmene
+  }
 }
