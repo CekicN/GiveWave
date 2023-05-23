a518ed12a1eca8fe121ee47759c732443662ac70
CekicN <cekicnikola53@gmail.com>
2023-05-23 13:00:33 +0200

    ProductList

--- Aplikacija/GiveWave/src/app/components/products/product-filter/product-filter.component.ts
+++ Aplikacija/GiveWave/src/app/components/products/product-filter/product-filter.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MyProductsFilterPipe } from 'app/pipes/my-products-filter.pipe';
 import { ProductService } from '../product.service';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-product-filter',
   templateUrl: './product-filter.component.html',
@@ -10,8 +12,11 @@ export class ProductFilterComponent implements OnInit{
   cities:any;
   status:string[] = ['New', 'Second hand']
   searchText = '';
-  constructor(private service:ProductService)
-  {}
+  constructor(private service:ProductService, library:FaIconLibrary)
+  {
+    library.addIcons(faMagnifyingGlass);
+  }
+
   ngOnInit(): void {
     this.service.getCities().subscribe(p => this.cities = p);
   }
--- Aplikacija/GiveWave/src/app/components/products/product-list/product-list.component.ts
+++ Aplikacija/GiveWave/src/app/components/products/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-list',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  products = ['Majca 2XL NOVO!!!','Farmerice 2XL NOVO!!!','Jakna 2XL NOVO!!!', 'Majca 2XL NOVO!!!' , 'Trenerka 2XL NOVO!!!']
+  constructor(library:FaIconLibrary)
+  {
+    library.addIcons(faShoppingCart);
+  }
+
+  addToCart(event:Event)
+  {
+    console.log(event.target);
+  }
 }
