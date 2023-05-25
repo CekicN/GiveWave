ab826dcd7d7a74278b7d0b98cada7857c3580e69
Stevan Ruzic <stevan.ruzic@elfak.rs>
2023-05-25 15:47:36 +0200

    asdad

---
+++ Aplikacija/GiveWaveAPI/GiveWaveAPI/Controllers/KorpaController.cs
@@ -0,0 +1,64 @@
+﻿using GiveWaveAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiveWaveAPI.Controllers
+{
+    public class KorpaController : ControllerBase
+    {
+        public GiveWaveDBContext Context { get; set; }
+        public KorpaController(GiveWaveDBContext context)
+        {
+            Context = context;
+        }
+
+        private List<Proizvod> proizvodiUKorpi;
+        
+        public KorpaController() 
+        {
+            proizvodiUKorpi = new List<Proizvod>();
+        }
+
+        [Route("PrikaziProizvode")]
+        [HttpGet]
+        [Authorize(Roles = "Prijatelj")]
+        public async Task<ActionResult> prikaziProizvode(EmailContent email)
+        {
+            try
+            {
+                var user = Context.ProfilKorisnikas.Where(p => p.Email == email.Name).FirstOrDefault();
+                if (user == null) 
+                {
+                    return BadRequest("Ne postoji user sa takvim email-om");
+                }
+                else
+                {
+                    return Ok(user.Proizvodi);
+                }
+              
+
+ 
+            }
+            catch(Exception ex) 
+            {
+                return BadRequest("Ugrozeni nije izabrao nijedan proizvod !");
+            }
+        }
+
+        [Route("DodajUKorpu")]
+        [HttpPost]
+        [Authorize(Roles = "Prijatelj")]
+        public async Task<ActionResult> dodajUKorpu(Proizvod proizvod)
+        {
+            try
+            {
+                proizvodiUKorpi.Add(proizvod);
+                return Ok("Dodali ste proizvod u korpi !");
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
